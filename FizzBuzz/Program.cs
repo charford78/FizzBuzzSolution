13c246e7e244f003f37e12f56cfed93eec5f890e
Charlie Ford <charles.ford1978@gmail.com>
2021-09-27 11:39:59 -0400

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var nbr2 = 1; nbr2 <= 30; nbr2++)
+            {
+                if (nbr2 % 3 == 0 && nbr2 % 5 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (nbr2 % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (nbr2 % 5 == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else
+                    Console.Write($"{nbr2} ");
+            }
+    }
+}
